d7ace2538555207e6d41d8987ab90ca04c813d06
Joshua Rogers <MegaManSec@users.noreply.github.com>
2025-10-08 15:46:53 +0800

    dtls1_shutdown: guard final SCTP SAVE_SHUTDOWN when wbio is NULL

--- ssl/d1_lib.c
+++ ssl/d1_lib.c
@@ -874,13 +874,14 @@ int dtls1_shutdown(SSL *s)
             return -1;
 
         if (ret == 0)
-            BIO_ctrl(SSL_get_wbio(s), BIO_CTRL_DGRAM_SCTP_SAVE_SHUTDOWN, 1,
-                     NULL);
+            BIO_ctrl(wbio, BIO_CTRL_DGRAM_SCTP_SAVE_SHUTDOWN, 1, NULL);
     }
 #endif
     ret = ssl3_shutdown(s);
 #ifndef OPENSSL_NO_SCTP
-    BIO_ctrl(SSL_get_wbio(s), BIO_CTRL_DGRAM_SCTP_SAVE_SHUTDOWN, 0, NULL);
+    if (wbio != NULL && BIO_dgram_is_sctp(wbio)) {
+        BIO_ctrl(wbio, BIO_CTRL_DGRAM_SCTP_SAVE_SHUTDOWN, 0, NULL);
+    }
 #endif
     return ret;
 }
